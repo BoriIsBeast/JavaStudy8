4bd2f19da919ef33d598dc3bf615971d511e2f6b
BoriIsBeast <wlsdn147@naver.com>
2022-01-19 17:24:50 +0900

    asd

---
+++ src/com/iu/s1/location/LocationDAO.java
@@ -0,0 +1,23 @@
+package com.iu.s1.location;
+
+public class LocationDAO {
+
+	public locationDAO{
+		
+		
+	}
+	public void getList() {
+		
+		//1. DB에 로그인
+		
+		//2. sql Query문 작성
+		
+		//3. 미리 전송
+		
+		//4. ? 세팅  // 패스!
+		
+		//5. 최종 전송 후 결과 처리
+		
+		//6. 자원 해제
+	}
+}
